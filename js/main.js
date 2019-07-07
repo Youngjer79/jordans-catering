e58619f9021f25b07a9d2866bcdc78fd92a0583b
Jermaine Young <jmaineyoung@yahoo.com>
2019-07-06 23:05:04 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-$(".carousel").carousel({
-  interval: 2000
+$(document).ready(function() {
+  $("btn-link").click(function() {
+    $("a").toggleClass("");
+  });
+});
+
+// Carousel Controls
+$(".carousel-control-prev").click(function() {
+  $("#slides").carousel("prev");
+});
+
+$(".carousel-control-next").click(function() {
+  $("#slides").carousel("next");
 });
